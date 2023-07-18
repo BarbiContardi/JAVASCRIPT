d7a3876e96ce0ab3acbd3321e7a72932aba3188b
Barbara Contardi <barbi.contardi@gmail.com>
2023-07-18 11:59:58 +0200

    Ejercicio 08

---
+++ es-8/exercise.js
@@ -0,0 +1,16 @@
+let y = true;
+
+console.log(y)
+console.log(typeof y);
+
+y = Number(y);
+console.log(y);
+console.log(typeof y);
+
+y = String(y);
+console.log(y);
+console.log(typeof y);
+
+y = Boolean(y);
+console.log(y);
+console.log(typeof y);
