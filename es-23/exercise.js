e6731705c964f29c3edca4cdf586fddb9a38501b
Barbara Contardi <barbi.contardi@gmail.com>
2023-07-19 12:39:10 +0200

    Ejercicio 23

---
+++ es-23/exercise.js
@@ -0,0 +1,15 @@
+let room = "Junior Suite Room"
+
+switch (room){
+    case "Basic Room":
+        console.log("El precio de la habitacion es 50€");
+    break;
+    case "Junior Suite Room":
+        console.log("El precio de la habitacion es 80€");
+    break;
+    case "Master Suite":
+        console.log("El precio de la habitacion es 100€");
+    break;    
+    default: 
+    console.log("La habitacion no existe");     
+}
