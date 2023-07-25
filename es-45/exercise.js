f28d3e0b1b45e3bac15d8b2c76c527e6165bfb85
Barbara Contardi <barbi.contardi@gmail.com>
2023-07-25 14:15:18 +0200

    Ejercicio 45

---
+++ es-45/exercise.js
@@ -0,0 +1,10 @@
+function Smartphone(brand,name,price){
+    this.brand = brand;
+    this.name = name;
+    this.price = price;
+}
+let smartphone2 = new Smartphone("Xiaomi", "pro 10", 700);
+let smartphone3 = new Smartphone ("Samsumg", "A 23", 1000)
+
+console.log(smartphone2);
+console.log(smartphone3)
