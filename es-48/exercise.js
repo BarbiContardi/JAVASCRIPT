733f694401eafa524415e6ac7ef0534bdbf33c00
Barbara Contardi <barbi.contardi@gmail.com>
2023-07-26 13:21:00 +0200

    Ejercicio 48

---
+++ es-48/exercise.js
@@ -0,0 +1,22 @@
+const students = [
+    {
+        id: 1,
+        name:"Olga",
+        surname: "Lopez",
+        age: 30,
+    },
+    {
+        id: 2,
+        name:"Ana",
+        surname: "Gil",
+        age: 35,
+    },
+    {
+        id: 3,
+        name:"Carlos",
+        surname: "Paez",
+        age: 25,
+    },
+]
+
+console.log(students[0])
