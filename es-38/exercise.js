7d07e63dd34d92db7ee604f3d75644a507026027
Barbara Contardi <barbi.contardi@gmail.com>
2023-07-26 12:46:46 +0200

    Ejercicio 38

--- es-38/exercise.js
+++ es-38/exercise.js
@@ -1,12 +1,10 @@
 function printName() {
-    let helloName = "Hello John";
-    setTimeout(function () {}, 1000);  //esto no me queda claro, me funciona pero me parece que no esta bien escrito
-  
-    function inner() {
-      console.log(helloName);
-    }
-  
-    inner();
+  let helloName = "Hello John";
+
+  function inner() {
+    console.log(helloName);
   }
-  printName();
-  
+  setTimeout(() => inner(), 1000);
+}
+
+printName();
