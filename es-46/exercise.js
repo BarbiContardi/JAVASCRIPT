e716294be7402d0ee2deb83ec5b0a1c994b4b287
Barbara Contardi <barbi.contardi@gmail.com>
2023-07-26 13:13:38 +0200

    Ejercicio 46

--- es-46/exercise.js
+++ es-46/exercise.js
@@ -2,4 +2,11 @@ const student = {
   id: 1,
   school: "Liceo",
   year: 3,
+
+  "personalData":{
+    name: "Carlos",
+    surname: "Lopez",
+    age: 32,
+  }
 };
+console.log(student.personalData)
