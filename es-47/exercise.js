2ccc51584ed6a7093149821f7a3c978a7ddab7e1
Barbara Contardi <barbi.contardi@gmail.com>
2023-07-26 13:14:28 +0200

    Ejercicio 47

---
+++ es-47/exercise.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers[0]);
+console.log(numbers[9]);
+console.log(numbers.length);
+console.log(numbers[20]);
+//En este caso la posicion nro 20 no existe, por eso nos dice undefined
+//siempre el indice del array empieza de 0,
+//entonces el 1 esta en la posicion 0 y el 10 en la posicion 9
